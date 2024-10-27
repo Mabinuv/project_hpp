a54619b4f1bdac27e1263b516b4b3c5d97cfb643
Mabin Varghese <mabinuv@gmail.com>
2024-10-27 23:07:38 +0100

    Phase-1 RFF

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,6 +3,12 @@ import pandas as pd
 import numpy as np
 
 def remove_unwanted_col(df, limit=0.5):
+    """
+    This function removes unwanted columns from a dataframe based on a threshold of NaN values
+    :param df: The dataframe to remove unwanted columns
+    :param limit: the percentage of NaN's in a column
+    :return: New dataset
+    """
     thr = limit * len(df)
     clean_df = df.dropna(axis=1, thresh=thr)
     return clean_df
